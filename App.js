91f69b3202fb57aafd23238eaef8bc71c7bb293d
rowa90ameen <159520497+rowa90ameen@users.noreply.github.com>
2024-11-06 20:17:05 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import LoginSignup from './components/loginSignup components/login';
+
+function App() {
+  return (
+    <div>
+      <LoginSignup/>
+    </div>
+  );
+}
+
+export default App;
